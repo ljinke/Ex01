557cb30e79de3276f215216e0293ccf4f02a636d
Jackie Liu <ljinke@gmail.com>
2013-12-04 10:38:08 +0800

    use dynamic to call method

--- CodeKata/spec/chop_test.rb
+++ CodeKata/spec/chop_test.rb
@@ -3,49 +3,34 @@ require_relative '../chop.rb'
 
 class TestChop < Test::Unit::TestCase
   def test_chop
-    assert_equal(-1, chop(3, []))
-    assert_equal(-1, chop(3, [1]))
-    assert_equal(0,  chop(1, [1]))
-    #
-    assert_equal(0,  chop(1, [1, 3, 5]))
-    assert_equal(1,  chop(3, [1, 3, 5]))
-    assert_equal(2,  chop(5, [1, 3, 5]))
-    assert_equal(-1, chop(0, [1, 3, 5]))
-    assert_equal(-1, chop(2, [1, 3, 5]))
-    assert_equal(-1, chop(4, [1, 3, 5]))
-    assert_equal(-1, chop(6, [1, 3, 5]))
-    #
-    assert_equal(0,  chop(1, [1, 3, 5, 7]))
-    assert_equal(1,  chop(3, [1, 3, 5, 7]))
-    assert_equal(2,  chop(5, [1, 3, 5, 7]))
-    assert_equal(3,  chop(7, [1, 3, 5, 7]))
-    assert_equal(-1, chop(0, [1, 3, 5, 7]))
-    assert_equal(-1, chop(2, [1, 3, 5, 7]))
-    assert_equal(-1, chop(4, [1, 3, 5, 7]))
-    assert_equal(-1, chop(6, [1, 3, 5, 7]))
-    assert_equal(-1, chop(8, [1, 3, 5, 7]))
+    inner_func(method(:chop))
   end
   def test_chop_recursive
-    assert_equal(-1, chop_recursive(3, []))
-    assert_equal(-1, chop_recursive(3, [1]))
-    assert_equal(0,  chop_recursive(1, [1]))
-    #
-    assert_equal(0,  chop_recursive(1, [1, 3, 5]))
-    assert_equal(1,  chop_recursive(3, [1, 3, 5]))
-    assert_equal(2,  chop_recursive(5, [1, 3, 5]))
-    assert_equal(-1, chop_recursive(0, [1, 3, 5]))
-    assert_equal(-1, chop_recursive(2, [1, 3, 5]))
-    assert_equal(-1, chop_recursive(4, [1, 3, 5]))
-    assert_equal(-1, chop_recursive(6, [1, 3, 5]))
-    #
-    assert_equal(0,  chop_recursive(1, [1, 3, 5, 7]))
-    assert_equal(1,  chop_recursive(3, [1, 3, 5, 7]))
-    assert_equal(2,  chop_recursive(5, [1, 3, 5, 7]))
-    assert_equal(3,  chop_recursive(7, [1, 3, 5, 7]))
-    assert_equal(-1, chop_recursive(0, [1, 3, 5, 7]))
-    assert_equal(-1, chop_recursive(2, [1, 3, 5, 7]))
-    assert_equal(-1, chop_recursive(4, [1, 3, 5, 7]))
-    assert_equal(-1, chop_recursive(6, [1, 3, 5, 7]))
-    assert_equal(-1, chop_recursive(8, [1, 3, 5, 7]))
+    inner_func(method(:chop_recursive))
   end
+  
+  private 
+    def inner_func(f)
+        assert_equal(-1, f.call(3, []))
+        assert_equal(-1, f.call(3, [1]))
+        assert_equal(0,  f.call(1, [1]))
+        #
+        assert_equal(0,  f.call(1, [1, 3, 5]))
+        assert_equal(1,  f.call(3, [1, 3, 5]))
+        assert_equal(2,  f.call(5, [1, 3, 5]))
+        assert_equal(-1, f.call(0, [1, 3, 5]))
+        assert_equal(-1, f.call(2, [1, 3, 5]))
+        assert_equal(-1, f.call(4, [1, 3, 5]))
+        assert_equal(-1, f.call(6, [1, 3, 5]))
+        #
+        assert_equal(0,  f.call(1, [1, 3, 5, 7]))
+        assert_equal(1,  f.call(3, [1, 3, 5, 7]))
+        assert_equal(2,  f.call(5, [1, 3, 5, 7]))
+        assert_equal(3,  f.call(7, [1, 3, 5, 7]))
+        assert_equal(-1, f.call(0, [1, 3, 5, 7]))
+        assert_equal(-1, f.call(2, [1, 3, 5, 7]))
+        assert_equal(-1, f.call(4, [1, 3, 5, 7]))
+        assert_equal(-1, f.call(6, [1, 3, 5, 7]))
+        assert_equal(-1, f.call(8, [1, 3, 5, 7]))
+    end
 end
--- knapsack.rb
+++ knapsack.rb
@@ -1,14 +1,24 @@
-#w:weight list
-#v:
+#Say there are n paintings with weights w1, ..., wn and market values v1, ..., vn. 
+#Define A(i,j) as the maximum value that can be attained from considering only the first i items weighting at most j pounds as follows. 
+#w:weight list of goods
+#v:maket values of goods
+#i:first i items
+#j:max weight of the bag
+#return: max value for the bag
 def A(w, v, i,j)
-  if i == 0 or j == 0
+  if i == 0 || j == 0
   	0
-	end
-  if w[i-1] > j
+	elsif w[i-1] > j
     A(w, v, i-1, j)
   else
-   	max(A(w,v, i-1, j), v[i-1] + A(w,v, i-1, j - w[i-1]))
+   	[A(w,v, i-1, j), v[i-1] + A(w,v, i-1, j - w[i-1])].max
   end
 end
 
-
+if __FILE__ == $0
+	w=[1,2,9,100,90,12,15,19,2,3,35,40,23,8,5] 
+	v=[1,3,90,1000,100,100,200,30,91,20,40,350,4000,32000,20]
+	memo = Array.new(v.length, Array.new(w.length,0))
+	p memo
+	p A(w,v,5,200)
+end
