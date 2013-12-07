83882627401f15ff7e2eaf6cd6e81802e92bac22
Jackie Liu <ljinke@gmail.com>
2013-12-07 11:15:10 +0800

    class variable vs. class instance variable

---
+++ double_at.rb
@@ -0,0 +1,57 @@
+#class variable vs. class instance variable
+class Polygon
+  class << self
+    attr_accessor :sides
+  end
+end
+
+class Triangle < Polygon
+  @sides = 3
+end
+
+class Rectangle < Polygon
+  @sides = 4
+end
+
+class Square < Rectangle
+end
+
+class Hexagon < Polygon
+  @sides = 6
+end
+if __FILE__ == $0
+	puts "Triangle.sides:  #{Triangle.sides.inspect}"  # 3
+	puts "Rectangle.sides: #{Rectangle.sides.inspect}" # 4
+	puts "Square.sides:    #{Square.sides.inspect}"    # nil
+	puts "Hexagon.sides:   #{Hexagon.sides.inspect}"   # 6
+end
+
+# why I don't like @@attributes
+class Base
+  @@shared = 'cattr from base'
+  class << self
+    attr_accessor :unshared
+  end
+  def self.shared
+    @@shared
+  end
+  self.unshared = 'attr from base'
+end
+class Derived < Base
+  @@shared = 'cattr from derived'
+  self.unshared = 'attr from derived'
+end
+class Ouch < Base
+  class << self
+    attr_accessor :unshared
+  end
+  def self.shared
+    @@shared
+  end
+end
+puts Base.unshared
+puts Base.shared
+puts Derived.unshared
+puts Derived.shared
+puts Ouch.unshared
+puts Ouch.shared
