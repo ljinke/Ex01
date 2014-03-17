ae2b3a916fbf6d3616c95ef563333e14c14678a7
liu jinkek <ljinke@gmail.com>
2014-03-17 10:03:40 +0800

    singleton methods

--- RubyBits/singleton_class.rb
+++ RubyBits/singleton_class.rb
@@ -2,8 +2,9 @@ if __FILE__ == $0
 
 	foobar = Array.new
 
+	# 1.
 	def foobar.size
-	  "Hello World!"
+	  super.to_s + "Hello World!"
 	end
 
 	puts foobar.size  # => "Hello World!"
@@ -12,10 +13,27 @@ if __FILE__ == $0
 	bizbat = Array.new
 	puts bizbat.size  # => 0
 
+	# 2
 	class << foobar
 		def singleton_test
 		end
 	end
+
+	#3
+	foobar.instance_eval do
+		self.class.send :define_method, :singleton_test3 do
+			p 'test 3'
+		end
+	end
+
+	#4 
+	foobar.instance_eval <<-EOS
+		def singleton_test2
+			p 'test2'
+		end
+	EOS
+
+	foobar.singleton_test3
 
 	puts '---------------------------'
 	puts foobar.methods
