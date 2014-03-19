730a527e7bfa816cef2ea53bebf8568425cce512
ljinke <ljinke@gmail.com>
2014-03-19 10:36:20 +0800

    join

--- array_max_by.rb
+++ array_max_by.rb
@@ -9,6 +9,8 @@ class A
 end
 
 if __FILE__ == $0
+
+
 	data = [A.new(1,"Tom"),A.new(2,"Mike"),A.new(2,"Jason")]
 	p data.max_by(&:id).id
 
@@ -19,5 +21,14 @@ if __FILE__ == $0
 	p data.select {|x| x.id%2 == 0}.collect {|x| x.name}
 
 	#p (1..100).select{|x| x ** 2 > 3 }.collect{|x| 2 * x}
+	p	data.find_all{ |x| x.id%2 == 0 }.length
+
+  p data.map {|x| x.name}.join
+
+  p data.collect {|x| x.name}.join
+
+  p A.name
+
+  p data[0].class.name
 end
 
