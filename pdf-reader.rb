095518515acbbd6ca2c74d2b4a955af989d88390
Jackie Liu <ljinke@gmail.com>
2014-05-16 07:23:16 +0800

    pdf-helper

--- pdf-reader.rb
+++ pdf-reader.rb
@@ -1,117 +1,159 @@
 require 'rubygems'
 require 'pdf/reader'
+require 'pdf-reader'
+
 
 class PdfHelper
-
-  # Check if we work
-  begin
-    require 'pdf-reader'
-    @@has_pdf_reader = true
-  rescue LoadError
-    @@has_pdf_reader = false
-  end
-
-  # Are PDF tools installed?
-  def PdfHelper.available?
-    @@has_pdf_reader
-  end
-
   # Given a PDF filename open it and extract out the links and return them as an 
   # array of hashes.  If they are a URL, they will have :url otherwise look for
   # :file.  Also :page is zero based and present and the bounding rectangle is in
   # the :rectangle return.
   def extract_links(filename)
-    raise "You must install pdf-reader gem!" unless @@has_pdf_reader
 
     # Open and go page by page
     links = []
-    reader = PDF::Reader.new(filename)
-    reader.pages.each_with_index do |page,i|
-      annots = page.attributes[:Annots]
-      next unless annots
+    begin
+      reader = PDF::Reader.new(filename)
 
-      # If we have annotations, go through each and find links
-      page.objects[annots].each do |annot|
-        next unless annot.is_a?(PDF::Reader::Reference)
-        annot_detail = reader.objects[annot]
-        next unless annot_detail[:Type] == :Annot && annot_detail[:Subtype] == :Link
+      root = reader.send(:root)
 
-        # For the links, find the sub-types and process them
-        annot_a_detail = annot_detail[:A].is_a?(PDF::Reader::Reference) ? reader.objects[annot_detail[:A]] : annot_detail[:A]
-        if annot_a_detail[:S] == :URI
-          links << {:page => i, :rectangle => annot_detail[:Rect].dup, :url => annot_a_detail[:URI]}
-        elsif annot_a_detail[:S] == :Launch or annot_a_detail[:S] == :GoToR
-          annot_file_detail = reader.objects[annot_a_detail[:F]]
-          links << {:page => i, :rectangle => annot_detail[:Rect].dup, :file => annot_file_detail[:UF]}
-        elsif annot_a_detail[:S] == :GoTo
-          # TODO - unsure how to handle these
-          p "Pdf::extract_links: ignoring GoTo link: (#{annot_a_detail.inspect})"
-          # annot_a_detail[:D].each do |goto_link|
-          #   annot_goto_details = reader.objects[goto_link]
-          # end
-        else
-          p "Pdf::extract_links: ignoring link type '#{annot_a_detail[:S]}': #{annot_a_detail.inspect} #{annot_detail.inspect}"
+      #p root
+      names = extract_reference(reader, root[:Names])
+      #p names
+
+      dests = extract_reference(reader, names[:Dests])
+      p dests
+
+      kids = extract_reference(reader, dests[:Kids])
+      p kids
+
+      real_names = extract_reference(reader, kids[0])[:Names]
+
+      p real_names
+
+      p get_page_number(reader, real_names[1])
+
+      reader.pages.each_with_index do |page,i|
+
+        annots = page.attributes[:Annots]
+        rotate = page.attributes[:Rotate] || 0
+
+
+        next unless annots
+
+        if annots.is_a?(PDF::Reader::Reference)
+          annots = page.objects[annots]
+        end
+        # If we have annotations, go through each and find links
+        annots.each do |annot|
+          next unless annot.is_a?(PDF::Reader::Reference)
+          annot_detail = reader.objects[annot]
+          next unless annot_detail[:Type] == :Annot && annot_detail[:Subtype] == :Link
+
+          # For the links, find the sub-types and process them
+          annot_a_detail = extract_reference(reader, annot_detail[:A])
+
+          if annot_a_detail[:S] == :URI
+            links << {:page => i, :rectangle => annot_detail[:Rect].dup, :url => extract_reference(reader, annot_a_detail[:URI]), :rotate => rotate}
+          elsif annot_a_detail[:S] == :Launch or annot_a_detail[:S] == :GoToR
+            annot_file_detail = reader.objects[annot_a_detail[:F]]
+            target_page = 0
+            destination = annot_a_detail[:D]
+            if destination
+              p "there is :GoToR destination"
+              if destination.is_a?(PDF::Reader::Reference)
+                p "GoToR destination is a object"
+                target_page = get_page_number(reader, destination)
+              elsif destination.is_a?(string)
+                p "It's a name destination"
+              else
+                p "GoToR destination is a page"
+                target_page = annot_a_detail[:D][0]
+              end
+            end
+            links << {:page => i, :rectangle => annot_detail[:Rect].dup, :file => annot_file_detail[:UF],:target_page => target_page, :rotate => rotate}
+          elsif annot_a_detail[:S] == :GoTo
+
+            destination = annot_a_detail[:D]
+            target_page = 0
+            case destination
+            when String #name destination
+            when PDF::Reader::Reference
+              if destination[0].is_a?(Fixnum)
+                target_page = destination[0]
+              else
+                target_page = get_page_number(destination[0])
+              end
+              
+            end
+
+            p "GoTo destination '#{annot_a_detail[:D]}'"
+            links << {:page => i, :rectangle => annot_detail[:Rect].dup, :file => filename, :target_page => target_page, :rotate => rotate}
+          else
+            p "Pdf::extract_links: ignoring link type '#{annot_a_detail[:S]}': #{annot_a_detail.inspect} #{annot_detail.inspect}"
+          end
         end
       end
+    rescue Exception => e
+      p "Pdf::extract_links: error occurd in extracting links: #{e}"
     end
     links
   end
 
-  def extract_page_dimensions(filename)
-    pages = []
-    reader = PDF::Reader.new(filename)
-    reader.pages.each do |page|
-      bbox   = page.attributes[:MediaBox]
-      width  = bbox[2] - bbox[0]
-      height = bbox[3] - bbox[1]
-      media_box = {:width=>width, :height=>height}
 
-      clip_box = {}
-      bbox   = page.attributes[:ClipBox]
-      if bbox
-        width  = bbox[2] - bbox[0]
-        height = bbox[3] - bbox[1]
-        clip_box = {:width=>width, :height=>height}
-      end
-
-      bleed_box = {}
-      bbox   = page.attributes[:BleedBox]
-      if bbox
-        width  = bbox[2] - bbox[0]
-        height = bbox[3] - bbox[1]
-        bleed_box = {:width=>width, :height=>height}
-      end
-
-      trim_box = {}
-      bbox   = page.attributes[:TrimBox]
-      if bbox
-        width  = bbox[2] - bbox[0]
-        height = bbox[3] - bbox[1]
-        trim_box = {:width=>width, :height=>height}
-      end
-
-      crop_box = {}
-      bbox   = page.attributes[:CropBox]
-      if bbox
-        width  = bbox[2] - bbox[0]
-        height = bbox[3] - bbox[1]
-        crop_box = {:width=>width, :height=>height}
-      end
-
-      p = {:MediaBox=>media_box, :ClipBox=>clip_box, :BleedBox=>bleed_box, :TrimBox=>trim_box, :CropBox=>crop_box}
-
-      pages << p
+  #get the origin coordiante from rotated values. PDF uses bottom-left as the orgin point(0,0), but its pages might be rotated which actually changes the (0,0) to top-left, top-right or bottom-right. The function will revised positions from the rotated view to top-left coordinate.
+  def self.rotate_rect(rect, original_width, original_height, degree = 0)
+    dpi_ratio =  2.08333333333333 # the ration between vv dpi and pdf dpi 150/72
+    width = original_width
+    height = original_height
+    x1 = rect[0]
+    y1 = rect[1]
+    x2 = rect[2]
+    y2 = rect[3]
+    result = case degree%360
+    when 0
+      rect
+    when 90
+      width = original_height
+      height = original_width
+      [y1, height - x1, y2, height - x2]
+    when 180
+      [width - x1, height - y1, width -x2, height - y2]
+    when 270
+      width = original_height
+      height = original_width
+      [width - y1, x1, width - y2,x2]
     end
-    pages
+
+    result = [result[0],height - result[1],result[2],height - result[3]]
+
+    result.map{|v| v * dpi_ratio}
   end
+
+  private
+    def extract_reference(reader, obj)
+      obj.is_a?(PDF::Reader::Reference) ? reader.objects[obj] : obj
+    end
+
+    def get_page_number(reader, obj)
+      p "get_page_number#obj=#{obj}"
+      if obj.is_a?(PDF::Reader::Page)
+        p "it's a page"
+        obj.number - 1 
+      else
+        reader.pages.each do |page|
+          if page.objects[obj]
+            p "found in page #{page.number}"
+            return page.number - 1 
+          end
+        end
+      end
+      0
+    end
 end
 
 if __FILE__ == $0
   pdf = PdfHelper.new
-	#links = pdf.extract_links("/home/jackie/Dropbox/Code Interview.pdf")
-	#links.each {|l| p l}
-
-  pages = pdf.extract_page_dimensions("/home/jackie/Downloads/PDF_with_Internal_Link_And_URL.pdf")
-
-  pages.each {|p| p p}
+	links = pdf.extract_links("/home/jackie/Downloads/a.pdf")
+  links.each {|l| p l}
 end
